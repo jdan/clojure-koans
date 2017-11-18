66f359a23b7360b0d3cf7606bdae8719743ac8e4
Jordan Scales <scalesjordan@gmail.com>
2017-11-18 11:54:47 -0800

    achieve enlightenment

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :profiles {:dev {:dependencies [[lein-koan "0.1.4"]]}}
   :repl-options {:init-ns koan-engine.runner
                  :init ^:displace (do (use '[koan-engine.core]))}
-  :plugins [[lein-koan "0.1.4"]]
+  :plugins [[lein-koan "0.1.4"]
+            [lein-cljfmt "0.5.7"]]
   :main koan-engine.runner/exec)
